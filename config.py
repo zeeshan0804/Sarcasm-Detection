c7b746f1c7945afaa6c90668817dfe908b41fd36
zeeshan0804 <67948488+zeeshan0804@users.noreply.github.com>
2024-10-13 02:00:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Config:
     def __init__(self):
         super().__init__()
-        self.word2id_path = "data/ghosh/word2id.txt"
-        self.train_path = "data/ghosh/train.txt"
-        self.test_path = "data/ghosh/test.txt"
-        self.model_save_path = "model/ghosh/model.pt"
-        self.train_loss_path = "loss_record/ghosh/train_loss_path.txt"
-        self.test_loss_path = "loss_record/ghosh/test_loss_path.txt"
+        self.word2id_path = "data/mishra/word2id.txt"
+        self.train_path = "data/mishra/train.txt"
+        self.test_path = "data/mishra/test.txt"
+        self.model_save_path = "model/mishra/model.pt"
+        self.train_loss_path = "loss_record/mishra/train_loss_path.txt"
+        self.test_loss_path = "loss_record/mishra/test_loss_path.txt"
         self.max_sen_len = 40      # Tweet句子最大长度为40 IAC的为60
         self.epoch = 200             #每个完整的数据集跑200轮
         self.batch_size = 16   #一个batch中放32个样例
